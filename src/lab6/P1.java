10c58b51c775fe59693fdeab5e7128e763a88298
Dominik Guziałek <tengongcreative@gmail.com>
2019-01-06 22:08:03 +0100

    Dodanie plików i podstawowych funkcji

--- src/lab6/P1.java
+++ src/lab6/P1.java
@@ -3,8 +3,11 @@ package lab6;
 public class P1 {
 
 	public static void main(String[] args) {
-		int a = 1;
+		int a = 1,b = 2;
 		System.out.println("TEST");
+		int w;
+		w = P2.dodawanie(a,b);
+		System.out.println(w);
 	}
 
 }
---
+++ src/lab6/P2.java
@@ -0,0 +1,10 @@
+package lab6;
+
+public class P2 {
+
+	static int dodawanie(int a, int b) {
+		int wynik = a + b;
+		return wynik;
+	}
+	
+}
