b4cceed8f0d1822c0a56b6832831d360c0f5b310
Dominik Guziałek <tengongcreative@gmail.com>
2019-01-06 23:45:56 +0100

    Dodanie dzielenia

--- src/lab6/P1.java
+++ src/lab6/P1.java
@@ -4,7 +4,7 @@ public class P1 {
 
 	public static void main(String[] args) {
 		int a = 1,b = 2;
-		System.out.println("TEST");
+		System.out.println("TEST");         ///Autor Dominik Guzia³ek
 		int w;
 		w = P2.dodawanie(a,b);
 		System.out.println(w + " " + P3.multi(a, b));
