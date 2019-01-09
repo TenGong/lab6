b2f74c9b1107ee5da04a76e2368dcef27e20d149
Dominik Guziałek <tengongcreative@gmail.com>
2019-01-09 10:48:41 +0100

    Naprawiłem kod sprawdający

--- src/lab6/L1.java
+++ src/lab6/L1.java
@@ -6,7 +6,7 @@ public class L1 {
 		
 	static String Czy_liczba(String a) {
 		String t = "";
-		boolean liczba = Pattern.matches("[0-9]", a);
+		boolean liczba = Pattern.matches(".*[0-9]", a);
 		if(liczba!=true) {
 			 t = "n";
 		} else {
